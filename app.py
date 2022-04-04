764cf61708fdf2102de9d71d7c0173983816c29f
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2022-04-04 22:07:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,33 @@ from flask import Flask, request, make_response
 import json
 import pickle
 
+
 app = Flask(__name__)
-with open('tokenizer.pickle', 'rb') as handle:
-    tokenizer = pickle.load(handle)
+# Data preparation, cleaning the text in the dataset
+
+def clean_txt(docs):
+
+    # 
+    lemmatizer = WordNetLemmatizer() 
+
+    speech_words = nltk.word_tokenize(docs)
+
+    lower_text = [w.lower() for w in speech_words]
+
+    re_punc = re.compile('[%s]' % re.escape(string.punctuation))
+    
+    stripped = [re_punc.sub('', w) for w in lower_text]
+    
+    words = [word for word in stripped if word.isalpha()]
+    
+    words = [w for w in words if not w in  list(STOP_WORDS)]
+    
+    words = [word for word in words if len(word) > 2]
+    
+    lem_words = [lemmatizer.lemmatize(word) for word in words]
+    combined_text = ' '.join(lem_words)
+    return combined_text
+
 model = pickle.load(open('intent_model.sav', 'rb'))
 
 @app.route('/')
@@ -33,7 +57,9 @@ def processRequest(req):    # This method processes the incoming request
     intent = result.get("intent").get('displayName')
     
     if (intent=='DataYes'):
-        prediction = model.predict([[symptom]])
+        for_pred = [clean_txt(symptom)]
+        
+        prediction = model.predict([[symptom]])[0
         output = round(prediction[0], 2)       
        
         fulfillmentText= "The right medical intent of what you are currently experiencing is:  {} !".format(output)
